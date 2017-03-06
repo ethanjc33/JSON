56f968156fcee86c98dae0e2758692187a2ab22d
Ethan Clark <ejc49@zips.uakron.edu>
2017-03-05 19:35:35 -0500

    Add files via upload
    Finished Array and Object parsing, weeded out last compiler errors

--- parse.cpp
+++ parse.cpp
@@ -8,16 +8,17 @@
 #include <cctype>
 #include <memory>
 #include <map>
+#include <iterator>
 
 
-//Concatenates two different int values to form one larger int
+//Concatenates the Num object with the next number in the string
 double concat(double p, int q) {
 	double hold = 1;
 	while (hold <= q) hold *= 10;
 	return (p * hold + q);
 }
 
-//Returns digits past the decimal point for number concatenation
+//Concatenates all numbers past a decimal point with their decimal value
 double toDouble(double p, int q) {
 	double hold = p;
 	double hold2 = q;
@@ -36,13 +37,15 @@ int toInt(std::string::iterator f) {
 //Classes
 struct value {
 	virtual ~value() { }
-	virtual void parse(std::string::iterator &f, std::vector<std::unique_ptr<value>> * v, int &weight) { }
+	virtual void parse(std::string::iterator &f, std::string::iterator l,
+					   std::vector<std::unique_ptr<value>> * v, int &weight) { }
 };
 
 struct Null : value {
 	Null() = default;
 
-	virtual void parse(std::string::iterator &f, std::vector<std::unique_ptr<value>> * v, int &weight) {
+	virtual void parse(std::string::iterator &f, std::string::iterator l,
+					   std::vector<std::unique_ptr<value>> * v, int &weight) {
 		int hold = v->size() + 1;
 		v->resize(hold);
 		v->at(hold).reset(new Null());
@@ -57,7 +60,8 @@ struct Boolean : value {
 	Boolean() = default;
 	Boolean(const bool & b) : cond(b) { }
 
-	void parse(std::string::iterator &f, std::vector<std::unique_ptr<value>> * v, int &weight) {
+	void parse(std::string::iterator &f, std::string::iterator l,
+			   std::vector<std::unique_ptr<value>> * v, int &weight) {
 		if (*f == 't') {
 			int hold = v->size() + 1;
 			v->resize(hold);
@@ -81,7 +85,8 @@ struct Num : value {
 
 	Num() = default;
 
-	virtual void parse(std::string::iterator &f, std::vector<std::unique_ptr<value>> * v, int &weight) {
+	virtual void parse(std::string::iterator &f, std::string::iterator l,
+					   std::vector<std::unique_ptr<value>> * v, int &weight) {
 		int x;
 		Num y;
 		while (isdigit(*f)) {
@@ -110,7 +115,8 @@ struct Num : value {
 struct String : value, std::string {
 	using std::string::string;
 
-	virtual void parse(std::string::iterator &f, std::vector<std::unique_ptr<value>> * v, int &weight) {
+	virtual void parse(std::string::iterator &f, std::string::iterator l,
+					   std::vector<std::unique_ptr<value>> * v, int &weight) {
 		String text;
 		while (*f != '\"') {
 			text += *f;
@@ -126,6 +132,7 @@ struct String : value, std::string {
 };
 
 struct Array : value, std::vector<value *> {
+	std::vector<std::unique_ptr<value>> * cont;
 	Array() = default;
 	~Array() {
 		for (value * v : *this) delete v;
@@ -133,17 +140,42 @@ struct Array : value, std::vector<value *> {
 
 	using std::vector<value *>::push_back;
 
-	virtual void parse(std::string::iterator &f, std::vector<std::unique_ptr<value>> * v, int &weight) {
-
+	virtual void parse(std::string::iterator &f, std::string::iterator l,
+					   std::vector<std::unique_ptr<value>> * v, int &weight) {
+		std::vector<std::unique_ptr<value>> u;
+		while (*f != ']') massParse(f, l, &u, weight);
+		Array * x = new Array();
+		x->cont = &u;
+		int hold = v->size() + 1;
+		v->resize(hold);
+		v->at(hold).reset(x);
+		++weight;
 	}
 };
 
 struct Object : value, std::map<std::string, value *> {
-	using std::map<std::string, value *>::map;
-	using std::map<std::string, value *>::insert;
+	std::map<String, value *> o;
 
-	virtual void parse(std::string::iterator &f, std::vector<std::unique_ptr<value>> * v, int &weight) {
+	Object() = default;
+	~Object() { o.clear(); }
 
+	virtual void parse(std::string::iterator &f, std::string::iterator l,
+					   std::vector<std::unique_ptr<value>> * v, int &weight) {
+		std::vector<std::unique_ptr<value>> u;
+		String text;
+		if (*f == '"') {
+			while (*f != '"') {
+				text += *f;
+				++f;
+			}
+		}
+		++f;
+		while (*f != '}') massParse(f, l, &u, weight);
+		Object * x = new Object();
+		int hold = v->size() + 1;
+		v->resize(hold);
+		v->at(hold).reset(x);
+		++weight;
 	}
 };
 
@@ -154,71 +186,76 @@ void skip(std::string::iterator & f, std::string::iterator l) {
 }
 
 
+void massParse(std::string::iterator & f, std::string::iterator l,
+			   std::vector<std::unique_ptr<value>> * v, int &weight) {
+	
+	skip(f, l);
+
+	if (*f == 'n') {
+		Null hold;
+		hold.parse(f, l, v, weight);
+		return;
+	}
+
+	else if (*f == 't' || *f == 'f') {
+		Boolean hold;
+		hold.parse(f, l, v, weight);
+		return;
+	}
+
+	else if (*f == '"') {
+		String hold;
+		hold.parse(f, l, v, weight);
+		return;
+	}
+
+	else if (isdigit(*f)) {
+		Num hold;
+		hold.parse(f, l, v, weight);
+		return;
+	}
+
+	else if (*f == '[') {
+		Array hold;
+		hold.parse(f, l, v, weight);
+		return;
+	}
+
+	else if (*f == '{') {
+		Object hold;
+		hold.parse(f, l, v, weight);
+		return;
+	}
+
+}
+
+
 //Application Implementation
 int main() {
 
-	//Obtains a string of JSON data from a text file
-	//NOTE: User must provide file format as "fileName.txt"
+	std::string name;						//Name of file
+	int weight = 0;							//Weight counter for JSON objects
+	std::vector<std::unique_ptr<value>> v;	//Vector will hold all JSON objects
 
-	int weight = 0;
-	std::string name, json;
-	std::vector<std::unique_ptr<value>> v;
-
+	//Obtain file name from user (NOTE: Must provide format as "filename.txt")
 	std::cout << "Enter JSON txt file:\n";
 	std::cin >> name;
 	std::ifstream file(name);
 
-	if (file.is_open()) {
-		while (std::getline(file, json)) file >> json;
-		file.close();
-	}
+	//String constructor takes two iterators, these iterators will read from beginning to end of file
+	std::string json{ std::istreambuf_iterator<char>(file), std::istreambuf_iterator<char>() };
 
-	else {
-		std::cout << "\nFile not Found\n";
-		return 0;
-	}
-
+	//Iterators for parsing through the entire JSON string
 	std::string::iterator f = json.begin();
 	std::string::iterator l = json.end();
 
-	while (f != l) {
-		std::cout << weight << '\n';
-		skip(f, l);
+	//Parsing loop looks at each character and determines which object to parse
+	while (f != l) massParse(f, l, &v, weight);
 
-		if (*f == 'n') {
-			Null hold;
-			hold.parse(f, &v, weight);
-			continue;
-		}
+	//Displays the JSON object's weight
+	std::cout << "\nParsing Completed...\nWeight: " << weight << '\n';
 
-		else if (*f == 't' || *f == 'f') {
-			Boolean hold;
-			hold.parse(f, &v, weight);
-			continue;
-		}
-
-		else if (*f == '"') {
-			String hold;
-			hold.parse(f, &v, weight);
-			continue;
-		}
-
-		else if (isdigit(*f)) {
-			Num hold;
-			hold.parse(f, &v, weight);
-			continue;
-		}
-
-		else if (*f == '[') {
-
-		}
-
-		else if (*f == '{') {
-
-		}
-
-	}
-
+	//TODO: Print out contents of vector - will sequentially read elements and print according to types
 
 	return 0;
 }
